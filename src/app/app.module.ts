646b0d318e79e782a37b64ef76c018bda1de92f4
singh44 <singh.vivek44@gmail.com>
2020-02-26 13:57:12 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
-
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
-
 import { AlertComponent } from './_components';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
-// import { AddUserComponent } from './user/add-user/add-user.component';
-// import { EditUserComponent } from './user/edit-user/edit-user.component';
-// import { ListUserComponent } from './user/list-user/list-user.component';
-
 import { UserFormModule } from './user/user.module';
-//import { APP_VERSION } from './tokens/app-version';
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -36,20 +28,12 @@ import { UserFormModule } from './user/user.module';
         HomeComponent,
         LoginComponent,
         RegisterComponent,
-        //AddUserComponent,
-        //EditUserComponent,
-        //ListUserComponent
-
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-       // { provide: APP_VERSION, useValue: '1.0.0'},
         // provider used to create fake backend
         fakeBackendProvider,
-
     ]
-   
 })
-
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-
 import { AlertService, AuthenticationService } from '../_services';
 
 @Component({
@@ -14,7 +13,6 @@ export class LoginComponent implements OnInit{
     loading = false;
     submitted = false;
     returnUrl: string;
-
     constructor(
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
@@ -27,28 +25,22 @@ export class LoginComponent implements OnInit{
             this.router.navigate(['/']);
         }
     }
-
     ngOnInit() {
         this.loginForm = this.formBuilder.group({
             username: ['', Validators.required],
             password: ['', Validators.required]
         });
-
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
-
     // convenience getter for easy access to form fields
     get f() { return this.loginForm.controls; }
-
     onSubmit() {
         this.submitted = true;
-
         // stop here if form is invalid
         if (this.loginForm.invalid) {
             return;
         }
-
         this.loading = true;
         this.authenticationService.login(this.f.username.value, this.f.password.value)
             .pipe(first())
